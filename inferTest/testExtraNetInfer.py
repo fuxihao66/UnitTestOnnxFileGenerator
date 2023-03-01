3de852f4736972caa69eed0a353b9f53516b592c
fuxihao66 <fuxihao66@gmail.com>
2023-03-01 23:41:20 +0900

    add

--- inferTest/testExtraNetInfer.py
+++ inferTest/testExtraNetInfer.py
@@ -1,36 +1,32 @@
-# https://github.com/onnx/models
 
 
 import onnxruntime as ort
 import numpy as np
 import cv2
 import time
-input_img = cv2.imread(r"D:\UGit\OnnxDMLPlugin\OnnxDMLTest\data\testimg.jpg") # h w c
-input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB )
-x = input_img.astype(np.float32)
+
 
 # x /= 255.
 # x_linear = np.power((x + 0.055)/1.055, 2.4).astype(np.float32)
 # cv2.imwrite("input_hdr.exr", x_linear)
-x = np.transpose(x, (2, 0, 1))
-x = x.reshape((1, 3, 224, 224))
+x = np.zeros((1, 21, 720, 1280), dtype=np.float16)
 
 sess_options = ort.SessionOptions()
-sess_options.enable_profiling = True
+# sess_options.enable_profiling = True
 # Set graph optimization level
 # sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
 sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
 
-ort_sess = ort.InferenceSession('D:/UGit/OnnxDMLPlugin/OnnxDMLTest/model/candy-9.onnx', sess_options, providers=["DmlExecutionProvider" ])#CPUExecutionProvider
+ort_sess = ort.InferenceSession('D:/UNetGated_UEIntegrate_fp16.onnx', sess_options, providers=["DmlExecutionProvider" ])#CPUExecutionProvider
 # ort_sess = ort.InferenceSession('D:/UGit/OnnxDMLPlugin/OnnxDMLTest/model/candy-9.onnx', providers=["CPUExecutionProvider" ])#
 # ort_sess = ort.InferenceSession('D:/UGit/OnnxDMLPlugin/OnnxDMLTest/model/candy-9.onnx', providers=["CUDAExecutionProvider" ])#
 start = time.time()
-# for i in range(1000):
-outputs = ort_sess.run(None, {'input1': x})
+for i in range(1000):
+    outputs = ort_sess.run(None, {'input_buffer': x})
 
-prof_file = ort_sess.end_profiling()
-print(prof_file)
-print("used time = {}".format((time.time() - start) / 1000.0))
+# prof_file = ort_sess.end_profiling()
+# print(prof_file)
+print("used time = {}".format((time.time() - start)))
 
 
 # output_img = np.transpose(outputs[0].reshape(3, 224, 224), (1, 2, 0)) 
